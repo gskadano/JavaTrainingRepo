8303653f1a21cf68e5df2ba95d6d6b1cbe97bec7
castich <castich@ZHA-ITA023-W7.corp.oocl.com>
2017-07-31 16:33:56 +0800

    Hello

--- src/main/java/JavaTraining/JAVA_TRAINING/App.java
+++ src/main/java/JavaTraining/JAVA_TRAINING/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello Genes!" );
         System.out.println( "Hello Geness!" );
+        System.out.println("Hay low");
     }
 }
